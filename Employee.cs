7f00a69a967d7cbfb1e76977aac77279a2714b59
Josué <gutillaj@gmail.com>
2023-01-27 00:34:28 -0400

    Cadastro de funcionário

---
+++ Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_lista
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void incleaseSalary(double porcentagem)
+        {
+            Salary += (Salary * porcentagem) / 100;
+        }
+
+        public override string ToString()
+        {
+            return Id 
+                + ", " 
+                + Name 
+                + ", " 
+                + Salary;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Metrics;
+using System.Globalization;
+
+namespace Exercicio_lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos funcionários serão cadastrados? ");
+            int quant = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            List<Employee> list = new List<Employee>();
+
+            for(int i  = 1; i <= quant; i++)
+            {
+                Console.WriteLine("Employee #" + i + ": ");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine());
+
+                list.Add(new Employee(id, nome, salario));
+            }
+
+            Console.WriteLine();
+            Console.Write("Informe o id do funcionário que terá aumento salarial: ");
+            double idAumento = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Employee emp = list.Find(x => x.Id == idAumento);
+
+            if(emp != null)
+            {
+                Console.Write("Digite a porcentagem: ");
+                double porcentagem = double.Parse(Console.ReadLine());
+                emp.incleaseSalary(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Esse id não existe!");
+            }
+
+            Console.WriteLine();
+            foreach(Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
